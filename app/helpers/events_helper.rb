04db15c32d6e041d4823aa35a391b4e760767932
enelesmai <xochitl.selene.fp@gmail.com>
2020-05-06 16:03:24 -0500

    added upcoming and previous events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,12 @@
 module EventsHelper
+
+    require 'date'
+
+    def upcoming_events
+        current_user.attended_events.select{|event| event.date >= Date.today}
+    end
+
+    def previous_events
+        current_user.attended_events.select{|event| event.date < Date.today}
+    end
 end
