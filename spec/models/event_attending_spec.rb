85483c442bd8bb63a610f05ab44289ca71d4e906
enelesmai <xochitl.selene.fp@gmail.com>
2020-05-07 10:49:23 -0500

    added testcase for eventattending

--- spec/models/event_attending_spec.rb
+++ spec/models/event_attending_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe EventAttending, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "create event attending" do
+    it "creates a new event attending" do
+      @user = User.new(:name => "User", :email => "Email")
+      @event = Event.new(:description => "Event", :date => "2020-05-07")
+      @eventattending = EventAttending.new(:event_attendee => @user, :attended_event => @event)
+ 
+      expect(@eventattending).to be_a_new(EventAttending)
+    end
+  end
 end
